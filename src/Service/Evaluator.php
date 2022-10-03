85a7ca8ce05f85ab18eaee9e6850565bf4b11a3c
pedrosobucki <pedrosobucki@gmail.com>
2022-10-02 23:11:52 -0300

    feat: added functionality for saving lowest bid value

--- src/Service/Evaluator.php
+++ src/Service/Evaluator.php
@@ -7,6 +7,7 @@ use PSobucki\Auction\Model\Auction;
 class Evaluator
 {
     private float $highestBid = -INF;
+    private float $lowestBid = INF;
 
     public function evaluate(Auction $auction): void
     {
@@ -14,11 +15,20 @@ class Evaluator
             if ($bid->getValue() > $this->highestBid) {
                 $this->highestBid = $bid->getValue();
             }
+
+            if ($bid->getValue() < $this->lowestBid) {
+                $this->lowestBid = $bid->getValue();
+            }
         }
     }
 
     public function getHighestBid(): float
     {
         return $this->highestBid;
+    }
+
+    public function getLowestBid(): float
+    {
+        return $this->lowestBid;
     }
 }
