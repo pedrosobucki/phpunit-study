b5f76c9884075c577bf873e38baa6c420ef849c2
pedrosobucki <pedrosobucki@gmail.com>
2022-10-02 23:12:33 -0300

    chore(tests): replicated 'highest bid' tests for new lowest bid values in Evaluator class

--- tests/Service/EvaluatorTest.php
+++ tests/Service/EvaluatorTest.php
@@ -57,4 +57,50 @@ class EvaluatorTest extends TestCase
         self::assertEquals(2500, $expectedValue);
     }
 
+    public function testAuctioneerShouldFindLowestBidInAscendingOrder(): void
+    {
+        // Arrange - Given;
+        $auction = new Auction("Fiat 147 0km");
+
+        $john = new User("John");
+        $anne = new User("Anne");
+
+        $auction->receiveBid(new Bid($john, 2000));
+        $auction->receiveBid(new Bid($anne, 2500));
+
+        $auctioneer = new Evaluator();
+
+
+        // Act - When
+        $auctioneer->evaluate($auction);
+        $expectedValue = $auctioneer->getLowestBid();
+
+
+        // Assert - Then
+        self::assertEquals(2000.0, $expectedValue);
+    }
+
+    public function testAuctioneerShouldFindLowestBidInDescendingOrder(): void
+    {
+        // Arrange - Given;
+        $auction = new Auction("Fiat 147 0km");
+
+        $john = new User("John");
+        $anne = new User("Anne");
+
+        $auction->receiveBid(new Bid($anne, 2500));
+        $auction->receiveBid(new Bid($john, 2000));
+
+        $auctioneer = new Evaluator();
+
+
+        // Act - When
+        $auctioneer->evaluate($auction);
+        $expectedValue = $auctioneer->getLowestBid();
+
+
+        // Assert - Then
+        self::assertEquals(2000.0, $expectedValue);
+    }
+
 }
