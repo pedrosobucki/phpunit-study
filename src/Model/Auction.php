121b349de21b6cf7846604de995d51b693ece0d1
pedrosobucki <pedrosobucki@gmail.com>
2022-10-04 00:08:10 -0300

    feat: Bids from same user cannot be added to Auction two times in a row

--- src/Model/Auction.php
+++ src/Model/Auction.php
@@ -16,6 +16,10 @@ class Auction
 
     public function receiveBid(Bid $bid): void
     {
+        if (!empty($this->bids) && $this->isRepeatedUserBid($bid)) {
+            return;
+        }
+
         $this->bids[] = $bid;
     }
 
@@ -25,5 +29,15 @@ class Auction
     public function getBids(): array
     {
         return $this->bids;
+    }
+
+    /**
+     * @param Bid $bid
+     * @return bool
+     */
+    public function isRepeatedUserBid(Bid $bid): bool
+    {
+        $lastBid = $this->bids[array_key_last($this->bids)];
+        return $lastBid->getUser() === $bid->getUser();
     }
 }
