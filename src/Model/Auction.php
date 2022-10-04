a5b1415ba43c5e494010d5209a2562dcad7a1b07
pedrosobucki <pedrosobucki@gmail.com>
2022-10-04 00:26:51 -0300

    feat: Allows maximum of 5 Bids per User in Auction

--- src/Model/Auction.php
+++ src/Model/Auction.php
@@ -4,6 +4,8 @@ namespace PSobucki\Auction\Model;
 
 class Auction
 {
+    public const MAX_BIDS_PER_USER = 5;
+
     /** @var Bid[] */
     private array $bids;
     private string $description;
@@ -17,6 +19,11 @@ class Auction
     public function receiveBid(Bid $bid): void
     {
         if (!empty($this->bids) && $this->isRepeatedUserBid($bid)) {
+            return;
+        }
+
+        $totalUserBids = $this->bidCountByUser($bid->getUser());
+        if ($totalUserBids >= self::MAX_BIDS_PER_USER) {
             return;
         }
 
@@ -35,5 +42,19 @@ class Auction
     {
         $lastBid = $this->bids[array_key_last($this->bids)];
         return $lastBid->getUser() === $bid->getUser();
+    }
+
+    private function bidCountByUser(User $user): int
+    {
+        return array_reduce(
+            $this->bids,
+            static function (int $accTotal, Bid $currentBid) use ($user) {
+                if ($currentBid->getUser() === $user) {
+                    return ++$accTotal;
+                }
+                return $accTotal;
+            },
+            0
+        );
     }
 }
