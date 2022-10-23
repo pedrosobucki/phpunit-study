1f7c0b92967fe99b22e648a69a20e67d998279ca
pedrosobucki <pedrosobucki@gmail.com>
2022-10-23 19:01:54 -0300

    feat: created Service class for closing Auctions

--- src/Service/Closer.php
+++ src/Service/Closer.php
@@ -2,7 +2,20 @@
 
 namespace PSobucki\Auction\Service;
 
+use PSobucki\Auction\DAO\AuctionDAO;
+
 class Closer
 {
+    public function close()
+    {
+        $dao = new AuctionDao();
+        $auctions = $dao->fetchNotClosed();
 
+        foreach ($auctions as $auction) {
+            if ($auction->wasCreatedMoreThanOneWeekAgo()) {
+                $auction->close();
+                $dao->update($auction);
+            }
+        }
+    }
 }
