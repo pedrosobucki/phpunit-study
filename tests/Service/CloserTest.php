c6d1d770bd8ec47d9d67095a153ee1a5b674a37a
pedrosobucki <pedrosobucki@gmail.com>
2022-10-23 20:22:57 -0300

    test: created AuctionDAO mock for testing Closer service class

--- tests/Service/CloserTest.php
+++ tests/Service/CloserTest.php
@@ -2,23 +2,61 @@
 
 namespace Psobucki\Auction\Tests\Service;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use PSobucki\Auction\DAO\AuctionDAO;
 use PSobucki\Auction\Model\Auction;
 use PSobucki\Auction\Service\Closer;
 
+class AuctionDAOMock extends AuctionDAO
+{
+    /** @var Auction[] */
+    private array $auctions = [];
+
+    public function save(Auction $auction): bool
+    {
+        $this->auctions[] = $auction;
+        return true;
+    }
+
+    /** @return Auction[] */
+    public function fetchClosed(): array
+    {
+        return array_filter($this->auctions,
+            static fn(Auction $auction) => $auction->isClosed()
+        );
+    }
+
+    /** @return Auction[] */
+    public function fetchNotClosed(): array
+    {
+        return array_filter($this->auctions,
+            static fn(Auction $auction) => !$auction->isClosed()
+        );
+    }
+
+    public function update(Auction $auction): bool
+    {
+        return true;
+    }
+
+}
+
 class CloserTest extends TestCase
 {
+    /**
+     * @throws Exception
+     */
     public function testMustCloseAuctionsStartedMoreThanOneWeekAgo(): void
     {
         $auctionFiat = new Auction('Fiat 147 0Km', new \DateTimeImmutable('8 days ago'));
         $auctionBeetle = new Auction('Blue Beetle', new \DateTimeImmutable('10 days ago'));
 
-        $auctionDAO = new AuctionDAO();
+        $auctionDAO = new AuctionDAOMock();
         $auctionDAO->save($auctionFiat);
         $auctionDAO->save($auctionBeetle);
 
-        $closer = new Closer();
+        $closer = new Closer($auctionDAO);
         $closer->close();
 
         $closedAuctions = $auctionDAO->fetchClosed();
