00438eace396cb37ec8f35520fb3f13646c628d2
pedrosobucki <pedrosobucki@gmail.com>
2022-10-23 19:01:27 -0300

    feat: created database connection and DAO class for Auction data

--- src/DAO/AuctionDAO.php
+++ src/DAO/AuctionDAO.php
@@ -2,7 +2,102 @@
 
 namespace PSobucki\Auction\DAO;
 
+use DateTimeImmutable;
+use PDO;
+use PSobucki\Auction\Infra\ConnectionCreator;
+use PSobucki\Auction\Model\Auction;
+
 class AuctionDAO
 {
+    private PDO $con;
 
+    private const INSERT = '
+        INSERT INTO leiloes (
+            descricao,
+            finalizado,
+            dataInicio
+        ) 
+        VALUES (?, ?, ?)
+    ';
+
+    private const FETCH_BASED_ON_CLOSED_STATUS = '
+        SELECT * 
+        FROM leiloes 
+        WHERE finalizado = ?
+    ';
+
+    private const UPDATE = '
+        UPDATE leiloes 
+        SET descricao = :descricao,
+            dataInicio = :dataInicio, 
+            finalizado = :finalizado 
+        WHERE id = :id
+    ';
+
+    public function __construct()
+    {
+        $this->con = ConnectionCreator::getConnection();
+    }
+
+    public function save(Auction $auction): bool
+    {
+        $stm = $this->con->prepare(self::INSERT);
+        $stm->bindValue(1, $auction->description(), PDO::PARAM_STR);
+        $stm->bindValue(2, $auction->isClosed(), PDO::PARAM_BOOL);
+        $stm->bindValue(3, $auction->startDate()->format('Y-m-d'));
+
+        return $stm->execute();
+    }
+
+    /**
+     * @return Auction[]
+     */
+    public function fetchNotClosed(): array
+    {
+        return $this->fetchBasedOnClosedStatus(false);
+    }
+
+    /**
+     * @return Auction[]
+     */
+    public function fetchClosed(): array
+    {
+        return $this->fetchBasedOnClosedStatus(true);
+    }
+
+    /**
+     * @return Auction[]
+     */
+    private function fetchBasedOnClosedStatus(bool $closed): array
+    {
+        $stm = $this->con->prepare(self::FETCH_BASED_ON_CLOSED_STATUS);
+        $stm->bindValue(1, $closed, \PDO::PARAM_BOOL);
+        $stm->setFetchMode(PDO::FETCH_ASSOC);
+
+        $stm->execute();
+        $auctions = [];
+
+        while($fetch = $stm->fetch(PDO::FETCH_ASSOC)) {
+            $auction = new Auction($fetch['descricao'], new DateTimeImmutable($fetch['dataInicio']), $fetch['id']);
+
+            if ($fetch['finalizado']) {
+                $auction->close();
+            }
+            $auctions[] = $auction;
+        }
+
+        return $auctions;
+    }
+
+    public function update(Auction $auction): bool
+    {
+        $stm = $this->con->prepare(self::UPDATE);
+
+        $stm->bindValue(':descricao', $auction->description());
+        $stm->bindValue(':dataInicio', $auction->startDate()->format('Y-m-d'));
+        $stm->bindValue(':finalizado', $auction->isClosed(), PDO::PARAM_BOOL);
+        $stm->bindValue(':id', $auction->id(), PDO::PARAM_INT);
+
+        return $stm->execute();
+    }
 }
--- src/Infra/ConnectionCreator.php
+++ src/Infra/ConnectionCreator.php
@@ -2,7 +2,20 @@
 
 namespace PSobucki\Auction\Infra;
 
+use PDO;
+
 class ConnectionCreator
 {
+    private static PDO $pdo;
 
+    public static function getConnection(): PDO
+    {
+        if (!isset(self::$pdo)) {
+            $databasePath = __DIR__ . '/../../db.sqlite';
+            self::$pdo = new PDO('sqlite:' . $databasePath);
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return self::$pdo;
+    }
 }
