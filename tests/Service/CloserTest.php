1592c82e124f29a3f916fb1a986c9a01c91b7780
pedrosobucki <pedrosobucki@gmail.com>
2022-10-24 23:09:51 -0300

    refactor: refactors CloserTest, initializing properties in setup

--- tests/Service/CloserTest.php
+++ tests/Service/CloserTest.php
@@ -3,47 +3,61 @@
 namespace Psobucki\Auction\Tests\Service;
 
 use Exception;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use PSobucki\Auction\DAO\AuctionDAO;
 use PSobucki\Auction\Model\Auction;
 use PSobucki\Auction\Service\Closer;
+use PSobucki\Auction\Service\MailSender;
 
 class CloserTest extends TestCase
 {
+    private Closer $closer;
+    private MockObject $mailSenderMock;
+    private Auction $auctionFiat;
+    private Auction $auctionBeetle;
+
+    /**
+     * @throws Exception
+     */
+    public function setUp(): void
+    {
+        $this->auctionFiat = new Auction('Fiat 147 0Km', new \DateTimeImmutable('8 days ago'));
+        $this->auctionBeetle = new Auction('Blue Beetle', new \DateTimeImmutable('10 days ago'));
+
+        $auctionDAO = $this->createMock(AuctionDAO::class);
+
+        $auctionDAO->method('fetchClosed')
+            ->willReturn([$this->auctionFiat, $this->auctionBeetle]);
+
+        $auctionDAO->expects(self::once())
+            ->method('fetchNotClosed')
+            ->willReturn([$this->auctionFiat, $this->auctionBeetle]);
+
+        $auctionDAO->expects($this->exactly(2))
+            ->method('update')
+            ->withConsecutive(
+                [$this->auctionFiat],
+                [$this->auctionBeetle]
+            );
+
+        $auctionDAO->fetchClosed();
+        $auctionDAO->save($this->auctionBeetle);
+
+        $this->mailSenderMock = $this->createMock(MailSender::class);
+        $this->closer = new Closer($auctionDAO, $this->mailSenderMock);
+    }
+
     /**
      * @throws Exception
      */
     public function testMustCloseAuctionsStartedMoreThanOneWeekAgo(): void
     {
-        $auctionFiat = new Auction('Fiat 147 0Km', new \DateTimeImmutable('8 days ago'));
-        $auctionBeetle = new Auction('Blue Beetle', new \DateTimeImmutable('10 days ago'));
+        $this->closer->close();
 
-        $auctionDAO = $this->createMock(AuctionDAO::class);
+        $closedAuctions = [$this->auctionFiat, $this->auctionBeetle];
 
-        $auctionDAO->method('fetchClosed')
-            ->willReturn([$auctionFiat, $auctionBeetle]);
-
-        $auctionDAO->expects(self::once())
-            ->method('fetchNotClosed')
-            ->willReturn([$auctionFiat, $auctionBeetle]);
-
-        $auctionDAO->expects($this->exactly(2))
-            ->method('update')
-            ->withConsecutive(
-                [$auctionFiat],
-                [$auctionBeetle]
-            );
-
-        $auctionDAO->fetchClosed();
-        $auctionDAO->save($auctionBeetle);
-
-        $closer = new Closer($auctionDAO);
-        $closer->close();
-
-        $closedAuctions = $auctionDAO->fetchClosed();
         static::assertCount(2, $closedAuctions);
-        static::assertEquals('Fiat 147 0Km', $closedAuctions[0]->description());
-        static::assertEquals('Blue Beetle', $closedAuctions[1]->description());
 
         static::assertTrue($closedAuctions[0]->isClosed());
         static::assertTrue($closedAuctions[1]->isClosed());
