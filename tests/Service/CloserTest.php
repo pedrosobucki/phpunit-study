00dd38e68b5c39679a6586c72a0f7dd190f60116
pedrosobucki <pedrosobucki@gmail.com>
2022-10-24 21:59:43 -0300

    test: removes internal class 'AuctionDAOMock' and created propper mock with PHPUnit 'createMock()' method

--- tests/Service/CloserTest.php
+++ tests/Service/CloserTest.php
@@ -8,40 +8,6 @@ use PSobucki\Auction\DAO\AuctionDAO;
 use PSobucki\Auction\Model\Auction;
 use PSobucki\Auction\Service\Closer;
 
-class AuctionDAOMock extends AuctionDAO
-{
-    /** @var Auction[] */
-    private array $auctions = [];
-
-    public function save(Auction $auction): bool
-    {
-        $this->auctions[] = $auction;
-        return true;
-    }
-
-    /** @return Auction[] */
-    public function fetchClosed(): array
-    {
-        return array_filter($this->auctions,
-            static fn(Auction $auction) => $auction->isClosed()
-        );
-    }
-
-    /** @return Auction[] */
-    public function fetchNotClosed(): array
-    {
-        return array_filter($this->auctions,
-            static fn(Auction $auction) => !$auction->isClosed()
-        );
-    }
-
-    public function update(Auction $auction): bool
-    {
-        return true;
-    }
-
-}
-
 class CloserTest extends TestCase
 {
     /**
@@ -52,8 +18,23 @@ class CloserTest extends TestCase
         $auctionFiat = new Auction('Fiat 147 0Km', new \DateTimeImmutable('8 days ago'));
         $auctionBeetle = new Auction('Blue Beetle', new \DateTimeImmutable('10 days ago'));
 
-        $auctionDAO = new AuctionDAOMock();
-        $auctionDAO->save($auctionFiat);
+        $auctionDAO = $this->createMock(AuctionDAO::class);
+
+        $auctionDAO->method('fetchClosed')
+            ->willReturn([$auctionFiat, $auctionBeetle]);
+
+        $auctionDAO->expects(self::once())
+            ->method('fetchNotClosed')
+            ->willReturn([$auctionFiat, $auctionBeetle]);
+
+        $auctionDAO->expects($this->exactly(2))
+            ->method('update')
+            ->withConsecutive(
+                [$auctionFiat],
+                [$auctionBeetle]
+            );
+
+        $auctionDAO->fetchClosed();
         $auctionDAO->save($auctionBeetle);
 
         $closer = new Closer($auctionDAO);
@@ -63,5 +44,8 @@ class CloserTest extends TestCase
         static::assertCount(2, $closedAuctions);
         static::assertEquals('Fiat 147 0Km', $closedAuctions[0]->description());
         static::assertEquals('Blue Beetle', $closedAuctions[1]->description());
+
+        static::assertTrue($closedAuctions[0]->isClosed());
+        static::assertTrue($closedAuctions[1]->isClosed());
     }
 }
